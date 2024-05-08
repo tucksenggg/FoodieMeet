b259f30bf5d87d185beb165640fa978a973fa83e
tucksenggg <tucksenggg@gmail.com>
2024-05-08 19:07:08 +0800

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function App() {
+  return <div>
+    <Header />
+    <h1 className="text-5xl font-bold underline">Hello</h1>
+    <Footer />
+  </div>
+}
+
+export default App;
