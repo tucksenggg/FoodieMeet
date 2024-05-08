959022330ee853f905cd9627c02c945a2eb92cc0
tucksenggg <tucksenggg@gmail.com>
2024-05-08 18:51:24 +0800

    added tailwind package

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended:true }));
 
-app.set('views', '../client/views');
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
 })
 
 app.listen(port, () => {
