d4fd97af3c8818051a67e1054eb8e9bc7d2d8c4c
tucksenggg <tucksenggg@gmail.com>
2024-05-09 14:07:20 +0800

    Updated Header

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import axios from 'axios';
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login"
+
 
 function App() {
-  return <div>
+
+  // const [backendData, setBackendData] = React.useState("");
+
+  // const apiCall = () => {
+  //   axios.get('http://localhost:3000').then((data) => {
+  //     setBackendData(data.data.message)
+  //     console.log(backendData);
+  //   })
+  // }
+
+  return (<div>
     <Header />
-    <h1 className="text-5xl font-bold underline">Hello</h1>
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+    </Routes>
     <Footer />
   </div>
+  )
 }
 
 export default App;
+
+// {/* <h1 className="text-5xl font-bold ">Hello</h1>
+//     <p>{backendData}</p>
+//     <button className="border-solid border-2 border-sky-500" onClick={apiCall}>Make API Call</button> */}
